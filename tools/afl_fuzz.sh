0bcc2a40783bb0c4dcdfc6d2490c32ce195ed425
Reima Ishii <ishiir@g.ecc.u-tokyo.ac.jp>
2023-06-28 16:47:27 +0900

    update

--- tools/afl_fuzz.sh
+++ tools/afl_fuzz.sh
@@ -14,6 +14,23 @@ elif [ "$2" = "M"  ]; then
 elif [ "$2" = "S"  ]; then
     tools/qemu_server "ivshmem_$3" "afl_bitmap_$3" > /dev/null 2>&1 &
     /home/ishii/nestedFuzz/AFLplusplus/afl-fuzz -i ./random_input -o "out/$1" -S fuzzer$3 -g 4096 -G 4096 -f afl_input_$3 -t 15000 -s 7 tools/fuzz_auto "ivshmem_$3" "afl_bitmap_$3" afl_input_$3
+elif [ -z "$2" ]; then
+    # tools/qemu_server "ivshmem_$2" "afl_bitmap_$2" > /dev/null 2>&1 &
+    /home/ishii/work/AFLplusplus/afl-fuzz -i ./random_input -o "out/$1" -g 4096 -G 4096 -f afl_input -t 15000 -s 7 tools/fuzz_auto "ivshmem" "afl_bitmap"
 else
-    /home/ishii/nestedFuzz/AFLplusplus/afl-fuzz -i ./random_input -o "out/$1" -g 4096 -G 4096 -f afl_input -t 15000 -s 7 tools/fuzz_auto
+    echo "Usage: afl_fuzz.sh <output_directory> <operation> <fuzzer_id>"
+    echo ""
+    echo "This script runs AFLplusplus with various configuration options."
+    echo ""
+    echo "Arguments:"
+    echo "<output_directory>  - Directory where AFL will write its output."
+    echo "<operation>         - The operation mode. Options are 'c', 'M', or 'S'."
+    echo "   'c' - Run the fuzzing job without additional coverage instrumentation and reporting."
+    echo "   'M' - Run the fuzzing job as the master process in a multi-fuzzing setup."
+    echo "   'S' - Run the fuzzing job as a secondary process in a multi-fuzzing setup."
+    echo "<fuzzer_id>         - An ID for the fuzzing process. It's used in the creation of shared memory segments and other identifiers."
+    echo ""
+    echo "Example:"
+    echo "  afl_fuzz.sh output_dir M 1"
+    echo "  This command will run the fuzzing job as the master process and output results to the 'output_dir' directory."
 fi
