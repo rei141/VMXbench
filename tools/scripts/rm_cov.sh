5f1b9cf67163de1de772d0c90affabb9f0094f68
rei141 <ishiir@g.ecc.u-tokyo.ac.jp>
2023-07-07 16:22:36 +0900

    add rm_cov.sh

---
+++ tools/scripts/rm_cov.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+source $SCRIPT_DIR/utilities.sh
+check_file $CONFIG_PATH
+WORK_DIR=$(python3 ./tools/scripts/get_yaml.py $CONFIG_PATH directories work_dir)
+
+sudo rm $WORK_DIR/total_kvm_arch_coverage $WORK_DIR/total_kvm_coverage -f
+sudo rm /dev/shm/kvm_arch_coverage /dev/shm/kvm_coverage -f
+
+if [ "$1" = "record"  ]; then
+    sudo rm $WORK_DIR/record/out/* $WORK_DIR/record/cov/* $WORK_DIR/record/*
+fi
